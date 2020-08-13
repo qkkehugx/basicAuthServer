b484a87c2612637ee7eef82b5f3eb56addfc4503
qkkehugx <renaldasjakas@gmail.com>
2020-08-14 00:54:22 +0100

    mongoose configuration

---
+++ mongooseConfig/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const configPath = './index.json';
+
+module.exports = (env) =>{
+  const validEnvironments = ['production', 'testing', 'development'];
+  if (!env || typeof env !== 'string') throw new Error('Invalid arguments passed');
+  if (!(validEnvironments.includes(env))) throw new Error('Invalid environment');
+  const parsed = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+  return parsed[env];
+};
